9e5b76c3985bcf0a9791479c70d15161ae687fec
Simon <simon.paillard@mail.mcgill.ca>
2016-04-30 16:44:15 -0400

    start authentication activity

---
+++ CTF.demo/app/src/main/java/com/example/ctfdemo/AuthenticatorActivity.java
@@ -0,0 +1,9 @@
+package com.example.ctfdemo;
+
+import android.accounts.AccountAuthenticatorActivity;
+
+/**
+ * Created by erasmas on 10/04/16.
+ */
+public class AuthenticatorActivity extends AccountAuthenticatorActivity {
+}
---
+++ CTF.demo/app/src/main/java/com/example/ctfdemo/StudentAuthenticator.java
@@ -0,0 +1,62 @@
+package com.example.ctfdemo;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by erasmas on 10/04/16.
+ */
+public class StudentAuthenticator extends AbstractAccountAuthenticator {
+
+    private Context mContext;
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+        return null;
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options) throws NetworkErrorException {
+        final Intent intent = new Intent(mContext, AuthenticatorActivity.class);
+        intent.putExtra(AuthenticatorActivity.ARG_ACCOUNT_TYPE, accountType);
+        intent.putExtra(AuthenticatorActivity.ARG_AUTH_TYPE, authTokenType);
+        intent.putExtra(AuthenticatorActivity.ARG_IS_ADDING_NEW_ACCOUNT, true);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+        return bundle;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+        final AccountManager am = AccountManager.get(mContext);
+        String authToken = am.peekAuthToken(account, authTokenType);  // TODO: add permissions in manifest
+
+    }
+
+    @Override
+    public String getAuthTokenLabel(String authTokenType) {
+        return null;
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
+        return null;
+    }
+}
