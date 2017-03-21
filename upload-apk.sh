2cf2eea6278404a410e56efc4c74c5f8f6c77838
Allan Wang <me@allanwang.ca>
2017-03-21 14:32:43 -0400

    final test

--- upload-apk.sh
+++ upload-apk.sh
@@ -3,9 +3,9 @@
 # https://github.com/Glucosio/glucosio-android/blob/develop/upload-gh-pages.sh
 
 #create a new directory that will contain our generated apk
-mkdir $HOME/android/
+mkdir $HOME/CTFA/
 #copy generated apk from build folder to the folder just created
-cp -R CTF/build/outputs/apk/CTF-Android.apk $HOME/android/
+cp -R CTF/build/outputs/apk/CTF-Android.apk $HOME/CTFA/
 #go to home and setup git
 cd $HOME
 git config --global user.email "me@allanwang.ca"
@@ -13,9 +13,9 @@ git config --global user.name "Allan Wang"
 #clone the repository in the buildApk folder
 git clone --quiet --branch=master  https://AllanWang:$GITHUB_API_KEY@github.com/CTFMcGill/CTFMcGill.github.io.git  master > /dev/null
 #go into directory and copy data we're interested
-cd master  cp -Rf $HOME/android/* .
+cd master
+cp -Rf $HOME/CTFA/* .
 #add, commit and push files
-git add -f .
 git remote rm origin
 git remote add origin https://AllanWang:$GITHUB_API_KEY@github.com/CTFMcGill/CTFMcGill.github.io.git
 git add -f .
