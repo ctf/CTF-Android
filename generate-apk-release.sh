2bc46d7c20466002d71e116a050a7ec8fb2e0cab
Allan Wang <me@allanwang.ca>
2017-03-21 16:47:01 -0400

    add v

--- generate-apk-release.sh
+++ generate-apk-release.sh
@@ -15,7 +15,7 @@ git clone --quiet --branch=master  https://AllanWang:$GITHUB_API_KEY@github.com/
 # create version file
 echo "Create Version File"
 cd master
-echo "CTFA $TRAVIS_BUILD_NUMBER" >> CTFA.txt
+echo "CTFA v$TRAVIS_BUILD_NUMBER" >> CTFA.txt
 
 echo "Push Version File"
 git remote rm origin
@@ -26,10 +26,10 @@ git push -fq origin master > /dev/null
 
 echo "Create New Release"
 cd $HOME
-API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "Android Release %s","draft": false,"prerelease": false}' $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER)
+API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Android Release v%s\nAutomatic Build","draft": false,"prerelease": false}' $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER)
 newRelease=$(curl --data "$API_JSON" https://api.github.com/repos/CTFMcGill/CTFMcGill.github.io/releases?access_token=$GITHUB_API_KEY)
 rID=`echo $newRelease | jq ".id"`
 curl -H "Content-Type:application/zip" \
--X POST --data "data=@CTFA/CTF-Android.apk" https://api.github.com/repos/CTFMcGill/CTFMcGill.github.io/releases/$rID/assets?access_token=$GITHUB_API_KEY&name=CTFA-$TRAVIS_BUILD_NUMBER.apk
+-X POST --data "data=@CTFA/CTF-Android.apk" https://api.github.com/repos/CTFMcGill/CTFMcGill.github.io/releases/$rID/assets?access_token=$GITHUB_API_KEY&name=CTFA-v$TRAVIS_BUILD_NUMBER.apk
 
 echo -e "Done\n"
