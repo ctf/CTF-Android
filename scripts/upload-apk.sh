21fa79fd78ea8a455b7915d7f0fc739bf5f7ea5a
AllanWang <me@allanwang.ca>
2017-03-21 03:57:10 -0400

    test 2

--- scripts/upload-apk.sh
+++ scripts/upload-apk.sh
@@ -1,19 +1,21 @@
+# https://medium.com/@daggerdwivedi/push-your-apk-to-your-github-repository-from-travis-11e397ec430d#.5v6g5lpxx
+
 #create a new directory that will contain out generated apk
 mkdir $HOME/buildApk/
 #copy generated apk from build folder to the folder just created
-cp -R CTF/build/outputs/apk/CTF-*.apk $HOME/android/
+cp -R CTF/build/outputs/apk/CTF-Android.apk $HOME/android/
 #go to home and setup git
 cd $HOME
 git config --global user.email "me@allanwang.ca"
 git config --global user.name "Allan Wang"
 #clone the repository in the buildApk folder
-git clone --quiet --branch=master  https://AllanWang:$GITHUB_API_KEY@github.com/CTFMcGill/CTF-Android  master > /dev/null
+git clone --quiet --branch=master  https://AllanWang:$GITHUB_API_KEY@github.com/CTFMcGill/CTFMcGill.github.io  master > /dev/null
 #go into directory and copy data we're interested
 cd master  cp -Rf $HOME/android/* .
 #add, commit and push files
 git add -f .
 git remote rm origin
-git remote add origin https://AllanWang:$GITHUB_API_KEY@github.com/CTFMcGill/CTF-Android.git
+git remote add origin https://AllanWang:$GITHUB_API_KEY@github.com/CTFMcGill/CTFMcGill.github.io.git
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
 git push -fq origin master > /dev/null
